4a8482cb827037c92efb2d624b22b112a6c7d789
Ever Winter <eternal.blizzard23@gmail.com>
2019-04-24 11:45:00 +0530

    knn.py: k-nearest neighbor classifier

---
+++ knn.py
@@ -0,0 +1,37 @@
+import numpy as numpy
+
+class KNN:
+    def __init__(self):
+        pass
+    
+    def train(self, X, Y):
+        self.X_train = X
+        self.Y_train = Y
+
+    def predict(self, X, k=1):
+        dist = self.compute_l2_dist(X)
+        return self.predict_labels(dists, k=k)
+
+    def compute_l2_dist(self, X):
+        n_test = X.shape[0]
+        n_train = self.X_train.shape[0]
+
+        dists = np.zeros((n_test, n_train))
+            dists = np.sqrt(-2 * np.dot(X, self.X_train.T) + np.sum(X**2, axis=1, 
+                    keepdims=True) + np.sum(self.X_train**2, axis=1))
+        
+        return dists
+
+    def predict_labels(self,  dists, k=1):
+        n_test = dists.shape[0]
+        Y_pred = np.zeros(n_test):
+
+        for i in range(n_test):
+            k_nearest_idxs = np.argsort(dists[i])[:k]
+            closest_y = self.Y_train[k_nearest_idxs]
+            Y_pred[i] = np.argmax(np.bincount(closest_y))
+        
+        return Y_pred
+        
+
+    
